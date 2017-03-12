ffbe04da0b0b1f30f75f5762a3ef1916d0f5fbe5
Thiago Bomfim <Thiago Bomfim>
2017-03-12 17:46:32 -0300

    Atualização main

--- src/main/java/br/net/mirante/modulo6/Main.java
+++ src/main/java/br/net/mirante/modulo6/Main.java
@@ -1,9 +1,21 @@
 package br.net.mirante.modulo6;
 
-public class Main {
+import java.sql.*;
 
-	public static void main(String[] args) {
-		System.out.println("Olá.");
+public class Main {
+	public static void main(String args[]) throws SQLException {
+		// Do something...
+		try {
+			Class.forName("org.hsqldb.jdbcDriver").newInstance();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		// Create the Connection:
+		Connection connection = DriverManager.getConnection("jdbc:hsqldb:/localhost/mydb", "sa", "secret");
+		System.out.println("opened");
+		// Use the connection...
+		// Close the connection:
+		connection.close();
+		System.out.println("closed");
 	}
-	
 }
