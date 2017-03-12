13e83c14e680d43c74e8eaf8b87a589e0bca8ebb
Thiago Bomfim <Thiago Bomfim>
2017-03-12 18:19:09 -0300

    Alteração no main

--- src/main/java/br/net/mirante/modulo6/Main.java
+++ src/main/java/br/net/mirante/modulo6/Main.java
@@ -3,20 +3,10 @@ package br.net.mirante.modulo6;
 import java.sql.*;
 
 public class Main {
-	public static void main(String args[]) throws SQLException {
-		// Do something...
-		try {
-			Class.forName("org.hsqldb.jdbcDriver").newInstance();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		// Create the Connection:
-		Connection connection = DriverManager.getConnection("jdbc:hsqldb:/localhost/mydb", "sa", "secret");
-		System.out.println("opened");
-		// Use the connection...
-		// Close the connection:
-		connection.close();
-		System.out.println("closed");
-		
+	public static void main(String args[]) {
+		JDBCConnectionPool pool = new JDBCConnectionPool("org.hsqldb.jdbcDriver", "jdbc:hsqldb://localhost/mydb", "sa",
+				"secret");
+		Connection con = pool.checkOut();
+		pool.checkIn(con);
 	}
 }
