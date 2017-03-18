28008aa194105346a1f9b72210d1bade3186e8ad
Thiago Bomfim <Thiago Bomfim>
2017-03-18 14:34:49 -0300

    main alterado

--- src/main/java/br/net/mirante/modulo6/Main.java
+++ src/main/java/br/net/mirante/modulo6/Main.java
@@ -3,6 +3,10 @@ package br.net.mirante.modulo6;
 import java.sql.*;
 
 public class Main {
+	/**
+	 * Adicionando comentario, pois não havia nenhum para ser removido.
+	 * @param args
+	 */
 	public static void main(String args[]) {
 		JDBCConnectionPool pool = new JDBCConnectionPool("org.hsqldb.jdbcDriver", "jdbc:hsqldb://localhost/mydb", "sa",
 				"secret");
